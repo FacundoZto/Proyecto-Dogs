8351e2c14e3ff61c7bf6ec2c85ea242b5ed73db8
facundozto <facundozerbato@outlook.com>
2024-04-15 18:35:49 -0300

    update dotenv

--- api/src/routes/data/dataFromApi.js
+++ api/src/routes/data/dataFromApi.js
@@ -1,27 +1,32 @@
-const axios = require('axios');
-
+require("dotenv").config();
+const axios = require("axios");
+const { API_KEY } = process.env;
 //TRAIGO LA DATA DE LA API
 
 const dataFromApi = async () => {
-	try{
-		const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds')
-				
-		const dataApi = apiUrl.data.map(d => {
-			return {
-				id: d.id,
-				name: d.name,
-				height: d.height.metric,
-				weight: d.weight.metric.includes('NaN') ? 'weight not found' : d.weight.metric,
-				life_span: d.life_span,
-				temperament: d.temperament,
-				image: d.image.url
-			}
-		})
-		
-		return dataApi;
-	}catch(err){
-		console.log(err)
-	}	
+  try {
+    const apiUrl = await axios.get(
+      `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
+    );
+
+    const dataApi = apiUrl.data.map((d) => {
+      return {
+        id: d.id,
+        name: d.name,
+        height: d.height.metric,
+        weight: d.weight.metric.includes("NaN")
+          ? "weight not found"
+          : d.weight.metric,
+        life_span: d.life_span,
+        temperament: d.temperament,
+        image: d.image?.url,
+      };
+    });
+
+    return dataApi;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = dataFromApi;
