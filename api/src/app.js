4a858dd07130badfb9c8d25134f2692ac67773bb
facundozto <facundozerbato@outlook.com>
2024-04-16 14:45:21 -0300

    app.js

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,6 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://proyecto-dogs-one.vercel.app",
     "https://dogs-ruddy-delta.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
