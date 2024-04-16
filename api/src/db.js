a1888402984992de0adecef42197bd022db36590
facundozto <facundozerbato@outlook.com>
2024-04-16 13:38:03 -0300

    pg

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+const pg = require("pg");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
 const sequelize = new Sequelize(
@@ -9,6 +10,7 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectModule: pg,
   }
 );
 const basename = path.basename(__filename);
