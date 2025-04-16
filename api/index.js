3388f59106166e6ace732667810882ef5fa5bca6
facundozto <facundozerbato@outlook.com>
2025-04-16 08:46:16 -0300

    fix redux store

--- api/index.js
+++ api/index.js
@@ -1,22 +1,3 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
@@ -27,7 +8,6 @@ conn.sync({ force: false }).then(() => { //conn es la instancia de sequelize
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
 
-    //console.log(conn.models);
     tempInDb(); //lleno la tabla temperamentos en la DB
   });
 });
